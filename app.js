06d133a737480da8b240bc221e76b7ac14a60912
Omer Moran <imomerm@gmail.com>
2018-03-04 19:05:58 +0200

    added clarifai

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const clarifai = require('./clarifai');
 
 const app = express();
 
@@ -14,5 +15,13 @@ app.get('/', (req, res) => {
   res.render('pages/index')
 });
 
+app.post('/predict', (req, res) => {
+  clarifai.predict(req.data.picture).then(
+    function(response) {
+      res.send(JSON.stringify(response.outputs[0].data.concepts));
+    }, function(err) {
+      res.send(err);
+    });
+});
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
