006e6052381ecddf05f379ee42e62fe34405217c
Omer Moran <imomerm@gmail.com>
2018-03-04 21:41:44 +0200

    kill me

--- public/index.js
+++ public/index.js
@@ -1,11 +1,6 @@
 var AlleRecipe = angular.module('AlleRecipe',['ngRoute']);
 
-<<<<<<< HEAD
-AlleRecipe.controller('mainctrl', function($scope) {
-  $scope.greeting = 'Hola!';
-=======
 AlleRecipe.controller('mainCtrl', ['$scope', function($scope) {
->>>>>>> 5c4731e267416c0e6bd499d826d7fb4feddee61a
   $scope.user = '';
   $scope.checkUser = function(){
     $scope.user= {name:"marina"};
@@ -22,11 +17,7 @@ AlleRecipe.controller('mainCtrl', ['$scope', function($scope) {
   })
   .when('/uploadingImage', {
     templateUrl: 'view/uploadingImage.html',
-<<<<<<< HEAD
     controller: 'uploadctrl'
-=======
-    controller : ""
->>>>>>> 5c4731e267416c0e6bd499d826d7fb4feddee61a
   })
   .when('/profile', {
     templateUrl: 'view/profile.html',
