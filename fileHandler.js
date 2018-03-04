deb89b9aa89a37f0417b42a91d4b2a45faa4f680
Nadav Halfon <nadav.halfon@gmail.com>
2018-03-04 22:13:42 +0200

    file handler

--- fileHandler.js
+++ fileHandler.js
@@ -6,9 +6,12 @@ module.exports.getImages = function() {
   return (JSON.parse(fs.readFileSync('data/images', 'utf8')));
 }
 
-module.export.addImage = function(img) {
+module.exports.addImage = function(img) {
   let images = fs.readFileSync('data/images');
   images = JSON.parse(images);
+  if (!images) {
+    images = [];
+  }
 
   images.push(img);
   fs.writeFileSync('data/images', JSON.stringify(images));
@@ -20,9 +23,12 @@ module.exports.getUsers = function() {
   return (JSON.parse(fs.readFileSync('data/users', 'utf8')));
 }
 
-module.export.addUser = function(user) {
+module.exports.addUser = function(user) {
   let users = fs.readFileSync('data/users');
   users = JSON.parse(users);
+  if (!users) {
+    users = [];
+  }
 
   images.push(user);
   fs.writeFileSync('data/users', JSON.stringify(users));
