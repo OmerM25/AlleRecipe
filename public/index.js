0e7032575eafd8b7cba07e131b2d136f7a399301
Omer Moran <imomerm@gmail.com>
2018-03-04 17:39:00 +0200

    asdffff

--- public/index.js
+++ public/index.js
@@ -2,13 +2,14 @@ var AlleRecipe = angular.module('AlleRecipe',[]);
 
 AlleRecipe.controller('GreetingController', ['$scope', function($scope) {
   $scope.greeting = 'Hola!';
-}])
-.config(function($routeProvider) {
-  $routeProvider
-   .when('/Book', {
-    templateUrl: 'a.html'
-  })
-  .when('/Books', {
-    templateUrl: 'index.html'
-  });
-});
+    alert("hey");
+}]);
+//.config(function($routeProvider) {
+//  $routeProvider
+//   .when('/Book', {
+//    templateUrl: 'a.html'
+//  })
+//  .when('/Books', {
+//    templateUrl: 'index.html'
+//  });
+//});
